7921ed68b8a90050cabdf5a1ef1eeb1cfdbeb4f9
Kenny Rachuonyo <krmboya@gmail.com>
2014-10-14 19:47:10 +0300

    add mapcar and append functions

--- text-game-engine.lisp
+++ text-game-engine.lisp
@@ -7,9 +7,13 @@
 (defun describe-location (location nodes)
   (cadr (assoc location nodes)))
 (defparameter *edges* '((living-room (garden west door)
-			 (attic upstair ladder))
+			 (attic upstairs ladder))
 			(garden (living-room east door))
 			(attic (living-room downstairs ladder))))
 (defun describe-path (edge)
   `(there is a ,(caddr edge) going ,(cadr edge) from here.)) ;; quasi-quoting
-			 
+
+
+(defun describe-paths (location edges)
+  (apply #'append (mapcar #'describe-path (cdr (assoc location edges)))))
+
