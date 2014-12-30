f154f5f922d854b56b090c4c7636017641404231
Kenny Rachuonyo <krmboya@gmail.com>
2014-12-31 01:27:06 +0300

    some more game functionality

--- text-game-engine.lisp
+++ text-game-engine.lisp
@@ -54,3 +54,25 @@
   (append (describe-location *location* *nodes*)
 	  (describe-paths *location* *edges*)
 	  (describe-objects *location* *objects* *object-locations*)))
+
+;; take a direction from current location, if it exists
+(defun walk (direction)
+  (let ((next (find direction
+		    (cdr (assoc *location* *edges*))
+		    :key #'cadr)))
+    (if next
+	(progn (setf *location* (car next))
+	       (look))
+	'(you cannot go that way.))))
+
+;; pick an item in location, if it exists
+(defun pickup (object)
+  (cond ((member object
+		 (objects-at *location* *objects* *object-locations*))
+	 (push (list object 'body) *object-locations*)
+	 `(you are now carrying ,object))
+	(t '(you cannot get that.))))
+
+;; show list of objects currently carrying
+(defun inventory ()
+  (cons 'items- (objects-at 'body *objects* *object-locations*)))
