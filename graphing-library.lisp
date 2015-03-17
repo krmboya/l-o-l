1397221ca15ac58836f16c7237902cc6b5dcde9e
Kenny Rachuonyo <krmboya@gmail.com>
2015-03-17 07:26:42 +0300

    add label generating function

--- graphing-library.lisp
+++ graphing-library.lisp
@@ -1,3 +1,14 @@
 ;; substitutes non-alphanumeric characters in node names to give valid DOT names
 (defun dot-name (exp)
   (substitute-if #\_ (complement #'alphanumericp) (prin1-to-string exp)))
+
+(defparameter *max-label-length* 30)
+
+;; generate a label of *max-label-length* that should appear on a graph node
+(defun dot-label (exp)
+  (if exp
+      (let ((s (write-to-string exp :pretty nil)))
+	(if (> (length s) *max-label-length*)
+	    (concatenate 'string (subseq s 0 (- *max-label-length* 3)) "...")
+	    s))
+      ""))
