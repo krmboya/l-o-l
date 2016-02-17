93b297e2f4aa4841f4d9d535da84666c9f46523e
Kenny Rachuonyo <krmboya@gmail.com>
2016-02-17 09:23:51 +0300

    added some player utils

--- orc-battle.lisp
+++ orc-battle.lisp
@@ -18,13 +18,34 @@
 (defun game-loop ()
   (unless (or (player-dead) (monsters-dead))
     (show-player)
+    ;; then player attacks k times (according to agility)
     (dotimes (k (1+ (truncate (/ (max 0 *player-agility*) 15))))
       (unless (monsters-dead)
 	(show-monsters)
 	(player-attack)))
     (fresh-line)
+    ;; each of the non-dead monsters attacks
     (map 'list
 	 (lambda (m)
 	   (or (monster-dead m) (monster-attack m)))
 	 *monsters*)
+    ;; loop again
     (game-loop)))
+
+
+(defun init-player ()
+  (setf *player-health* 30)
+  (setf *player-agility* 30)
+  (setf *player-strength* 30))
+
+(defun player-dead ()
+  (<= *player-health* 0))
+
+(defun show-player ()
+  (fresh-line)
+  (princ "You are a valiant knight with a health of ")
+  (princ *player-health*)
+  (princ ", an agility of ")
+  (princ *player-agility*)
+  (princ ", and a strength of ")
+  (princ *player-strength*))
