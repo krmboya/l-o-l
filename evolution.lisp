d84fda5fe3762cf7776491d0ebaa5e84434568f8
Kenny Rachuonyo <krmboya@gmail.com>
2016-06-16 07:23:24 +0300

    begin animal functionality

--- evolution.lisp
+++ evolution.lisp
@@ -16,3 +16,34 @@
 (defun add-plants ()
    (apply #'random-plant *jungle*)
    (random-plant 0 0 *width* *height*))
+
+;; Structure describing animal
+(defstruct animal x y energy dir genes)
+
+;; initialize variable animals with a list containing one animal
+(defparameter *animals* 
+    (list (make-animal :x      (ash *width*  -1)
+                       :y      (ash *height* -1)
+                       :energy 1000
+                       :dir    0
+                       :genes  (loop repeat 8
+				  collecting (1+ (random 10))))))
+
+
+(defun move (animal)
+  (let ((dir (animal-dir animal))
+        (x (animal-x animal))
+        (y (animal-y animal)))
+    (setf (animal-x animal) (mod (+ x
+                                    (cond ((and (>= dir 2) (< dir 5)) 1)
+                                          ((or (= dir 1) (= dir 5)) 0)
+                                          (t -1))
+                                    *width*)
+                                 *width*))
+    (setf (animal-y animal) (mod (+ y
+                                    (cond ((and (>= dir 0) (< dir 3)) -1)
+                                          ((and (>= dir 4) (< dir 7)) 1)
+                                          (t 0))
+                                    *height*)
+                                 *height*))
+    (decf (animal-energy animal))))
