4f1c9bccdb7e1baeeb7e1cfee6e8a13b1eb62fe5
Kenny Rachuonyo <krmboya@gmail.com>
2016-08-18 12:52:56 +0300

    notes on decode-param

--- webserver.lisp
+++ webserver.lisp
@@ -11,11 +11,24 @@
 ; Parts Adapted with permission from http.lisp by Ron Garret
 
 (defun decode-param (s)
-   (labels ((f (lst)
-               (when lst
-                 (case (car lst)
-                     (#\% (cons (code-char (parse-integer (coerce (list (cadr lst) (caddr lst)) 'string) :radix 16 :junk-allowed t))
-                                (f (cdddr lst))))
-                     (#\+ (cons #\space (f (cdr lst))))
-                     (otherwise (cons (car lst) (f (cdr lst))))))))
-       (coerce (f (coerce s 'list)) 'string)))
+  "Recursively decodes a URL encoded string"
+  (labels ((f (lst)
+	     (when lst
+	       ;; check the first character
+	       (case (car lst)
+
+		 ;; if % convert following hex number to a character i.e.
+		 ;; next two characters
+		 ;; if invalid, returns nil (junk-allowed)
+		 (#\% (cons (code-char (parse-integer (coerce (list 
+							       (cadr lst) 
+							       (caddr lst)) 
+							      'string) 
+						      :radix 16 
+						      :junk-allowed t))
+			    (f (cdddr lst))))
+		 ;; if + replace with space
+		 (#\+ (cons #\space (f (cdr lst))))
+		 (otherwise (cons (car lst) (f (cdr lst))))))))
+    ;; convert string to list, process it, then back to string
+    (coerce (f (coerce s 'list)) 'string)))
